e8298d2a9d623dc92c7f74bf6e876c3afe4a2f1c
jraymer <joeygatacre4@gmail.com>
2022-12-13 00:15:36 -0800

    adds terminal icons import

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -3,4 +3,6 @@ Invoke-Expression (&starship init powershell)
 # install command --> PowerShellGet\Install-Module posh-git -Scope CurrentUser
 Import-Module posh-git
 
+Import-Module Terminal-Icons
+
 New-Alias touch ni
